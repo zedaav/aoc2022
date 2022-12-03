df830b28fbae5fd2741e35931aec6a76c6ed0f8d
David Pochet <david.pochet@gmail.com>
2022-12-03 11:12:36 +0100

    Solution for day 3

---
+++ src/aoc2022/day03.py
@@ -0,0 +1,94 @@
+import logging
+from functools import lru_cache
+from pathlib import Path
+
+from aoc2022.puzzle import AOCPuzzle
+
+"""
+Solutions for https://adventofcode.com/2022/day/3
+"""
+
+# Common codes
+BIG_A_CODE = ord("A")
+SMALL_A_CODE = ord("a")
+
+
+# Reckon item priority
+@lru_cache
+def priority(item: str) -> int:
+    # Reckon priority from common element
+    code = ord(item)
+    if code >= SMALL_A_CODE:
+        return code - SMALL_A_CODE + 1
+    return code - BIG_A_CODE + 27
+
+
+# Puzzle class
+class D03Puzzle(AOCPuzzle):
+    def __init__(self, input_file: Path):
+        # Prepare puzzle data
+        self.common_items = []
+
+        # Super call
+        super().__init__(input_file)
+
+    def solve(self) -> int:
+        # Sum the priorities
+        return sum(priority(c) for c in self.common_items)
+
+
+# Step 1 class
+class D03Step1Puzzle(D03Puzzle):
+    def parse_line(self, index: int, line: str) -> str:
+        # Super call
+        parsed_line = super().parse_line(index, line)
+
+        # Parse content string
+        content_len = len(parsed_line)
+        assert content_len % 2 == 0, f"Error while parsing bag content at line {index}: length is not even ({parsed_line})"
+        half_len = int(content_len / 2)
+        first_compartment = parsed_line[:half_len]
+        second_compartment = parsed_line[half_len:]
+
+        # Find common element
+        common = None
+        for element in first_compartment:  # pragma: no branch
+            if element in second_compartment:
+                common = element
+                break
+        assert common is not None, f"Common element not found at line {index}"
+
+        # Ok!
+        logging.info(f"New bag at line {index}; common element: {common}; priority: {priority(common)}")
+        self.common_items.append(common)
+
+        return parsed_line
+
+
+# Step 2 class
+class D03Step2Puzzle(D03Puzzle):
+    def __init__(self, input_file: Path):
+        # Prepare puzzle data
+        self.group_of_3 = []
+
+        # Super call
+        super().__init__(input_file)
+
+    def parse_line(self, index: int, line: str) -> str:
+        # Super call
+        parsed_line = super().parse_line(index, line)
+
+        # Add to group
+        self.group_of_3.append(parsed_line)
+        if len(self.group_of_3) == 3:
+            # Time to look for common item
+            common_set = set(self.group_of_3[0]) & set(self.group_of_3[1]) & set(self.group_of_3[2])
+            assert len(common_set) == 1, f"Can't find a common element at line {index}: {self.group_of_3} / {common_set}"
+            common = common_set.pop()
+            logging.info(f"New common item at line {index}: {common}; priority: {priority(common)}")
+            self.common_items.append(common)
+
+            # Clear group
+            self.group_of_3.clear()
+
+        return parsed_line
