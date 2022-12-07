8dec9d0c6a968ebff830485808c5751ffa86fe44
David Pochet <david.pochet@gmail.com>
2022-12-07 08:01:46 +0000

    Solution for day 7

--- src/aoc2022/day07.py
+++ src/aoc2022/day07.py
@@ -1,3 +1,5 @@
+import logging
+import re
 from pathlib import Path
 from typing import Union
 
@@ -7,31 +9,90 @@ from aoc2022.puzzle import AOCPuzzle
 Solutions for https://adventofcode.com/2022/day/7
 """
 
+# Prefixes & patterns
+PREFIX_CD = "$ cd "
+PREFIX_LS = "$ ls"
+PREFIX_DIR = "dir "
+FILE_PATTERN = re.compile("([0-9]+) +.*")
+ROOT_PATH = "/"
+
 
 # Puzzle class
 class D07Puzzle(AOCPuzzle):
     def __init__(self, input_file: Path):
         # Init puzzle data
+        self.cwd = Path(ROOT_PATH)
+        self.sizes = {}
+        self.handle_new_dir()
+        self.grab_sizes = False
+        self.ls_done = set()
 
         # Super call
         super().__init__(input_file)
+
+    def handle_new_dir(self):
+        # Already known?
+        if self.cwd not in self.sizes:
+            logging.info(f"Handle new directory: {self.cwd}")
+            self.sizes[self.cwd] = 0
 
     def parse_line(self, index: int, line: str) -> str:
         # Super call
         parsed_line = super().parse_line(index, line)
 
-        return parsed_line
+        # Checks commands
+        if parsed_line.startswith(PREFIX_CD):
+            # Change directory
+            new_dir = parsed_line[len(PREFIX_CD) :]
+            if new_dir == ROOT_PATH:
+                self.cwd = Path(ROOT_PATH)
+            else:
+                self.cwd = (self.cwd / new_dir).resolve()
+            self.handle_new_dir()
+        elif parsed_line.startswith(PREFIX_LS):
+            # Just the ls command; toggle size grabbing only if not done yet for this path
+            if self.cwd not in self.ls_done:
+                logging.info(f"Start grabbing sizes for {self.cwd}")
+                self.ls_done.add(self.cwd)
+                self.grab_sizes = True
+            else:
+                self.grab_sizes = False
+        elif parsed_line.startswith(PREFIX_DIR):
+            # New dir... but actually we don't care
+            pass
+        else:
+            # Last case is size reckoning
+            m = FILE_PATTERN.match(parsed_line)
+            assert m is not None
+            new_size = int(m.group(1))
+            if self.grab_sizes:
+                # Add this size to cwd and all parents
+                path_to_add = self.cwd
+                while True:
+                    self.sizes[path_to_add] += new_size
+                    logging.info(f" >> Add {new_size} to {path_to_add} -- new size is {self.sizes[path_to_add]}")
+                    if path_to_add == Path(ROOT_PATH):
+                        break
+                    else:
+                        path_to_add = path_to_add.parent
 
-    def solve(self) -> Union[int, str]:
-        # Solution is...
-        return 0
+        return parsed_line
 
 
 # Step 1 class
 class D07Step1Puzzle(D07Puzzle):
-    pass
+    def solve(self) -> Union[int, str]:
+        # Solution is sum of sizes < 100000
+        return sum(filter(lambda s: s <= 100000, self.sizes.values()))
 
 
 # Step 2 class
 class D07Step2Puzzle(D07Puzzle):
-    pass
+    def solve(self) -> Union[int, str]:
+        # Currently used space
+        current_used = self.sizes[Path(ROOT_PATH)]
+        current_free = 70000000 - current_used
+        need_to_free = 30000000 - current_free
+
+        # Solution is sum of sizes < 100000
+        return min(filter(lambda s: s >= need_to_free, self.sizes.values()))
--- src/tests/test_day07.py
+++ src/tests/test_day07.py
@@ -4,13 +4,13 @@ from tests.base import AOCPuzzleTester
 
 class TestD07(AOCPuzzleTester):
     def test_step1_sample(self):
-        self.check_solution(D07Step1Puzzle, "d07.sample.txt", 0)
+        self.check_solution(D07Step1Puzzle, "d07.sample.txt", 95437)
 
     def test_step1_input(self):
-        self.check_solution(D07Step1Puzzle, "d07.input.txt", 0)
+        self.check_solution(D07Step1Puzzle, "d07.input.txt", 1543140)
 
     def test_step2_sample(self):
-        self.check_solution(D07Step2Puzzle, "d07.sample.txt", 0)
+        self.check_solution(D07Step2Puzzle, "d07.sample.txt", 24933642)
 
     def test_step2_input(self):
-        self.check_solution(D07Step2Puzzle, "d07.input.txt", 0)
+        self.check_solution(D07Step2Puzzle, "d07.input.txt", 1117448)
