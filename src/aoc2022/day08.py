e48f3708a8f09f030d14f807af8eb75c930e1f00
David Pochet <david.pochet@gmail.com>
2022-12-08 09:45:42 +0100

    Definitive solution for day 8

--- src/aoc2022/day08.py
+++ src/aoc2022/day08.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import List, Union
 
+import numpy
+
 from aoc2022.puzzle import AOCPuzzle
 
 """
@@ -19,9 +21,6 @@ class Tree:
     top_ones: List[int]
     bottom_ones: List[int]
 
-    def __repr__(self) -> str:
-        return f"Tree(v: {self.value}, left:{self.left_ones}, right:{self.right_ones}, top:{self.top_ones}, bottom:{self.bottom_ones})"
-
 
 # Puzzle class
 class D08Puzzle(AOCPuzzle):
@@ -33,38 +32,27 @@ class D08Puzzle(AOCPuzzle):
         # Super call
         super().__init__(input_file)
 
+        # Build an array with everything
+        tree_array = numpy.array(self.int_lines)
+        logging.info(f"Built array:\n{tree_array}")
+
+        # Browse all trees
+        for y in range(len(self.int_lines)):
+            for x in range(len(self.int_lines[0])):
+                self.trees[(x, y)] = Tree(
+                    tree_array[y][x],  # Tree value (= height)
+                    tree_array[y][0:x][::-1],  # Trees on the left (from closest to farest)
+                    tree_array[y][x + 1 :],  # Trees on the right
+                    tree_array[:, x][0:y][::-1],  # Trees on the top (from closest to farest)
+                    tree_array[:, x][y + 1 :],  # Trees on the bottom
+                )
+
     def parse_line(self, index: int, line: str) -> str:
         # Super call
         trimmed_line = super().parse_line(index, line)
-        logging.info(f"handling new line {index}")
 
-        # Browse each tree in the line
-        int_line = [int(c) for c in trimmed_line]
-        y = index
-        for x, tree_value in enumerate(int_line):
-            # Persist in map:
-            # * tree coordinates (x,y) as keys
-            # * tree items (tree value, list of all neighbor trees) as values
-
-            # 1. trees on the left
-            left_ones = int_line[0:x]
-
-            # 2. trees on the right
-            right_ones = int_line[x + 1 :]
-
-            # 3. trees on the top
-            top_ones = [ln[x] for ln in self.int_lines]
-
-            # 4. compute trees on the bottom for all already processed trees on the same column
-            for (_x, _y), t in filter(lambda t: t[0][0] == x, self.trees.items()):
-                t.bottom_ones.append(tree_value)
-
-            # Remember tree
-            new_t = Tree(tree_value, left_ones, right_ones, top_ones, [])
-            self.trees[(x, y)] = new_t
-
-        # Remember line of trees
-        self.int_lines.append(int_line)
+        # New line
+        self.int_lines.append([int(c) for c in trimmed_line])
 
         return trimmed_line
 
@@ -74,8 +62,7 @@ class D08Step1Puzzle(D08Puzzle):
     def solve(self) -> Union[int, str]:
         # Count all trees greater than at least one of its neighbors lists
         count = 0
-        for (x, y), t in self.trees.items():
-            logging.info(f"Check tree at ({x}, {y}): {t}")
+        for (_x, _y), t in self.trees.items():
             for n in [t.left_ones, t.right_ones, t.top_ones, t.bottom_ones]:
                 if not len(n) or (t.value > max(n)):
                     count += 1
@@ -86,4 +73,26 @@ class D08Step1Puzzle(D08Puzzle):
 
 # Step 2 class
 class D08Step2Puzzle(D08Puzzle):
-    pass
+    def solve(self) -> Union[int, str]:
+        # Browse all trees not on the borders
+        top_scenic_score = 0
+        for y in range(1, len(self.int_lines) - 1):
+            for x in range(1, len(self.int_lines[0]) - 1):
+                # Get distance to closest neighbor tree at least as tall as current tree, in all directions
+                tree = self.trees[(x, y)]
+                scenic_score = 1
+                for trees in [tree.left_ones, tree.right_ones, tree.top_ones, tree.bottom_ones]:
+                    # Default distance is distance to the border
+                    max_dist = len(trees)
+                    for dist, neighbor in enumerate(trees, 1):
+                        if neighbor >= tree.value:
+                            # Closer distance: a neighbor tree is at least as tall as current tree
+                            max_dist = dist
+                            break
+                    scenic_score *= max_dist
+
+                # Better score?
+                if scenic_score > top_scenic_score:
+                    top_scenic_score = scenic_score
+
+        return top_scenic_score
--- src/tests/test_day08.py
+++ src/tests/test_day08.py
@@ -7,10 +7,10 @@ class TestD08(AOCPuzzleTester):
         self.check_solution(D08Step1Puzzle, "d08.sample.txt", 21)
 
     def test_step1_input(self):
-        self.check_solution(D08Step1Puzzle, "d08.input.txt", 0)
+        self.check_solution(D08Step1Puzzle, "d08.input.txt", 1849)
 
     def test_step2_sample(self):
-        self.check_solution(D08Step2Puzzle, "d08.sample.txt", 0)
+        self.check_solution(D08Step2Puzzle, "d08.sample.txt", 8)
 
     def test_step2_input(self):
-        self.check_solution(D08Step2Puzzle, "d08.input.txt", 0)
+        self.check_solution(D08Step2Puzzle, "d08.input.txt", 201600)
