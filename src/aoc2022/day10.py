ce68f4ba29375ce2eef81f801aca3142714c1085
David Pochet <david.pochet@gmail.com>
2022-12-10 12:32:46 +0100

    Solution for day 10

--- src/aoc2022/day10.py
+++ src/aoc2022/day10.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 from typing import Union
 
@@ -7,11 +8,17 @@ from aoc2022.puzzle import AOCPuzzle
 Solutions for https://adventofcode.com/2022/day/10
 """
 
+# Instructions prefixes
+INST_ADD = "addx "
+INST_NOP = "noop"
+
 
 # Puzzle class
 class D10Puzzle(AOCPuzzle):
     def __init__(self, input_file: Path):
         # Init puzzle data
+        self.current_value = 1
+        self.values = []
 
         # Super call
         super().__init__(input_file)
@@ -20,18 +27,53 @@ class D10Puzzle(AOCPuzzle):
         # Super call
         trimmed_line = super().parse_line(index, line)
 
-        return trimmed_line
+        # Check instruction
+        if trimmed_line.startswith(INST_NOP):
+            # Just add one cycle with same value
+            self.values.append(self.current_value)
+        else:
+            # Add two cycles with previous values, then change value
+            self.values.append(self.current_value)
+            self.values.append(self.current_value)
+            self.current_value += int(trimmed_line[len(INST_ADD) :])
 
-    def solve(self) -> Union[int, str]:
-        # Solution is...
-        return 0
+        return trimmed_line
 
 
 # Step 1 class
 class D10Step1Puzzle(D10Puzzle):
-    pass
+    def solve(self) -> Union[int, str]:
+        # Sample values
+        result = 0
+        for sample in [20, 60, 100, 140, 180, 220]:
+            register = self.values[sample - 1]
+            logging.info(f"Register value at cycle {sample}: {register}")
+            result += register * sample
+        return result
 
 
 # Step 2 class
 class D10Step2Puzzle(D10Puzzle):
-    pass
+    def solve(self) -> Union[int, str]:
+        # Render image
+        line_length = 40
+        rendered_line = ""
+        output = []
+        for cycle in range(240):
+            # Is register matching cycle position?
+            register = self.values[cycle]
+            sprite_left = register - 1
+            sprite_right = register + 1
+            pixel = cycle % line_length  # NOQA: S001
+            if (pixel >= sprite_left) and (pixel <= sprite_right):
+                rendered_line += "#"
+            else:
+                rendered_line += "."
+
+            # One more line?
+            if (cycle % line_length) == (line_length - 1):  # NOQA: S001
+                output.append(rendered_line)
+                logging.info(f"Rendered line: {rendered_line}")
+                rendered_line = ""
+
+        return output
--- src/tests/test_day10.py
+++ src/tests/test_day10.py
@@ -4,13 +4,29 @@ from tests.base import AOCPuzzleTester
 
 class TestD10(AOCPuzzleTester):
     def test_step1_sample(self):
-        self.check_solution(D10Step1Puzzle, "d10.sample.txt", 0)
+        self.check_solution(D10Step1Puzzle, "d10.sample.txt", 13140)
 
     def test_step1_input(self):
-        self.check_solution(D10Step1Puzzle, "d10.input.txt", 0)
+        self.check_solution(D10Step1Puzzle, "d10.input.txt", 10760)
 
     def test_step2_sample(self):
-        self.check_solution(D10Step2Puzzle, "d10.sample.txt", 0)
+        solution = [
+            "##..##..##..##..##..##..##..##..##..##..",
+            "###...###...###...###...###...###...###.",
+            "####....####....####....####....####....",
+            "#####.....#####.....#####.....#####.....",
+            "######......######......######......####",
+            "#######.......#######.......#######.....",
+        ]
+        self.check_solution(D10Step2Puzzle, "d10.sample.txt", solution)
 
     def test_step2_input(self):
-        self.check_solution(D10Step2Puzzle, "d10.input.txt", 0)
+        solution = [
+            "####.###...##..###..#..#.####..##..#..#.",
+            "#....#..#.#..#.#..#.#..#.#....#..#.#..#.",
+            "###..#..#.#....#..#.####.###..#....####.",
+            "#....###..#.##.###..#..#.#....#.##.#..#.",
+            "#....#....#..#.#....#..#.#....#..#.#..#.",
+            "#....#.....###.#....#..#.#.....###.#..#.",
+        ]
+        self.check_solution(D10Step2Puzzle, "d10.input.txt", solution)
