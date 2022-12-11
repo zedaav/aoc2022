465b66e783d0f38f25e4fa7fce56f21f9df9aa36
David Pochet <david.pochet@gmail.com>
2022-12-11 16:28:32 +0100

    Skeleton for day 12

---
+++ src/tests/test_day12.py
@@ -0,0 +1,16 @@
+from aoc2022.day12 import D12Step1Puzzle, D12Step2Puzzle
+from tests.base import AOCPuzzleTester
+
+
+class TestD12(AOCPuzzleTester):
+    def test_step1_sample(self):
+        self.check_solution(D12Step1Puzzle, "d12.sample.txt", 0)
+
+    def test_step1_input(self):
+        self.check_solution(D12Step1Puzzle, "d12.input.txt", 0)
+
+    def test_step2_sample(self):
+        self.check_solution(D12Step2Puzzle, "d12.sample.txt", 0)
+
+    def test_step2_input(self):
+        self.check_solution(D12Step2Puzzle, "d12.input.txt", 0)
