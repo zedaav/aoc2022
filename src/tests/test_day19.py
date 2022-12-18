8822639d6a5111686ea3c8f9ca3f727a4942ba7d
David Pochet <david.pochet@gmail.com>
2022-12-18 14:40:18 +0100

    Skeleton for day 19

---
+++ src/tests/test_day19.py
@@ -0,0 +1,16 @@
+from aoc2022.day19 import D19Step1Puzzle, D19Step2Puzzle
+from tests.base import AOCPuzzleTester
+
+
+class TestD19(AOCPuzzleTester):
+    def test_step1_sample(self):
+        self.check_solution(D19Step1Puzzle, "d19.sample.txt", 0)
+
+    def test_step1_input(self):
+        self.check_solution(D19Step1Puzzle, "d19.input.txt", 0)
+
+    def test_step2_sample(self):
+        self.check_solution(D19Step2Puzzle, "d19.sample.txt", 0)
+
+    def test_step2_input(self):
+        self.check_solution(D19Step2Puzzle, "d19.input.txt", 0)
